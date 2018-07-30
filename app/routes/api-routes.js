9d9ea5f9df2fd8d4d8b1dea15021fd5e5e9f2e97
Eva Lieu <itsevalieu@gmail.com>
2018-07-29 22:32:12 -0700

    Adding cors, setting up in index.js

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,7 +1,7 @@
 //Dependencies
 //============================================================
 const path = require("path");
-
+const os = require("os");
 //Data
 //============================================================
 const teas = require("../data/teas.json");
@@ -10,8 +10,12 @@ const teas = require("../data/teas.json");
 //============================================================
 
 module.exports = function(app){
-	app.get("/", function(request, response){
-		response.send({"hello": "world"});
+	app.get("/", function(request, response, next){
+		response.json({
+			"mess": "hello, you're on the whitelist",
+			"origin": request.headers.origin,
+			"os_hostname": os.hostname(),
+			"os_cpus": os.cpus()
 	});
 	app.get("/api", function(request, response){
 		console.log(teas);
--- index.js
+++ index.js
@@ -1,14 +1,61 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+const conf = {
+	port: process.env.PORT || process.argv[2] || 3000,
+	originUndefined: function (req, res, next) {
+		if (!req.headers.origin) {
+			res.json({
+				"mess": "Hey, you're visiting the service locally. If this was a CORS the origin header should not be undefined"
+			});
+		} else {
+			next();
+		}
+	},
+	cors: {
+		origin: function (origin, cb) {
+			let whiteList = ['https://teafographic.netlify.com'];
+			if(whiteList.indexOf(origin) != -1) {
+				cb(null, true);
+			} else {
+				cb(new Error('invalid origin: ' + origin), false);
+			}
+		},
+		optionsSuccessStatus: 200
+	}
+};
+app.use(conf.originUndefined, cors(conf.cors));
+// const allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+//     // intercept OPTIONS method
+//     if ('OPTIONS' == req.method) {
+//       res.send(200);
+//     }
+//     else {
+//       next();
+//     }
+// };
+
+
+
+// app.configure(function () {
+// 	app.use(allowCrossDomain);
+// 	app.use(express.bodyParser());
+// 	app.use(express.methodOverride());
+// 	app.use(app.router);
+// 	app.use(express.static(path.join(application_root, "public")));
+// 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+// });
 
 //Router
 //============================================================
@@ -17,6 +64,6 @@ require("./app/routes/api-routes.js")(app);
 
 //Listener
 //============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(conf.port, function() {
+  console.log("App listening on PORT " + conf.port);
 });
