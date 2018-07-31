e3d3b5213ddc53b9856702bb2764de218d00b5a5
Eva Lieu <itsevalieu@gmail.com>
2018-07-30 18:09:07 -0700

    Adding app.use(cors())

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
