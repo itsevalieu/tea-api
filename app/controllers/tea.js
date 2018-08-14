70b5bd28694ca209fb43c9459abbbf896c462b5b
Eva Lieu <itsevalieu@gmail.com>
2018-08-13 21:54:35 -0700

    CRUD operations for teas working

--- app/controllers/tea.js
+++ app/controllers/tea.js
@@ -1,11 +1,14 @@
 const Tea = require('../models/Tea.js');
 const teas = require('../data/teas.json');
-
+	// .get(function(request, response){
+	// 	console.log(teas);
+	// 	response.json(teas);
+	// });
 module.exports = {
     getTea: async (req, res, next) => {
-        //Get all teas
-        console.log('did it?');
-        res.status(200).json({success: 'Got dem tea!'});
+        const data = await Tea.find({});
+        console.log('got tea');
+        res.status(200).json({teas});
     },
     postTea: async (req, res, next) => {
         const { 
@@ -17,8 +20,8 @@ module.exports = {
             origin,
             imageUrl,
             handle
-        } = req.value.body;
-        
+        } = req.body;
+
         //create new tea
         const newTea = new Tea({
             name,
@@ -31,8 +34,41 @@ module.exports = {
             handle
         });
         await newTea.save();
-        console.log('working?');
-        //respond with tea
-        res.status(200).json({success: 'success!'});
+        //respond with message
+        res.status(200).json({success: 'Successfully added to database!'});
+    },
+    updateTea: async (req, res, next) => {
+        const { 
+            name,
+            description,
+            benefits,
+            type,
+            brew,
+            origin,
+            imageUrl,
+            handle
+        } = req.body;
+
+        const foundTea = await Tea.findByIdAndUpdate({
+            _id: req.params.id
+        }, { 
+            $set: {
+                name,
+                description,
+                benefits,
+                type,
+                brew,
+                origin,
+                imageUrl,
+                handle
+            }
+        }, (err) => {
+            if(err) return err;
+        });
+        res.status(200).send({success: 'Successfully updated'});
+    },
+    deleteTea: async (req, res, next) => {
+        const foundTea = await Tea.findByIdAndDelete({_id: req.params.id}); 
+        res.status(200).json({success: 'Successfully deleted.'});
     }
 }
